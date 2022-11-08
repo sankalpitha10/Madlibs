f09cdcfb307f72b96100eaef30dad5bb3edb4fe3
Sankalpitha Yerrapragada <1997sankalpitha@gmail.com>
2022-11-08 11:18:02 +0000

    Add files via upload

---
+++ Libs.py
@@ -0,0 +1,18 @@
+
+A_tv_show= input("Enter a tv show:")
+First_color=input("Enter First color:")
+second_colorr=input("Enter second color:")
+Third_color=input("Enter Third color:")
+Fourth_color=input("Enter Fourth color:")
+A_type_of_chair=input("Enter a type of chair:")
+Country=input("Enter a country:")
+Animal=input("Enter an Animal:")
+choose = input("Enter your choice(Mother, Father):")
+choose_one=input("Enter your chice(Monday, Tuesday, Wednesday, Thursday, Friday):")
+lunch=input("Enter lunch:")
+video_game=input("enter your fav video game:")
+car_model=input("Enter car model:")
+
+
+
+print('As I watched my favourite T.V. show'  +A_tv_show+ 'I sat down in my' +First_color+ 'When the show was over, I started flipping through the channels. The news caught my eye. I heard that ' +Animal+ 'had been lost from the Central Zoo and was now roaming around in' +Country+ 'Lunch! called my' +choose+ 'from the kitchen. It was' +choose_one+ ' so we were having' +lunch+  'for lunch. After lunch, I watched my mother play ' +video_game+ 'Bedtime!" called my mother, "Tomorrow were going to the Zoo!" The next morning I put on my'  +second_colorr+  'T-shirt and my jeans. I thought about what the people had said on the news. I started to get worried. When we got in the'  + car_model+  ' On the way there I saw the colours of'  +Third_color+  'and'  +Fourth_color+  ' whisk by.')
